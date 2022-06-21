47e5f4c7f8f9e3fab433bd91967cd66ba6cdda8a
brenofroes <bfroes58@gmail.com>
2022-06-20 23:08:31 -0300

    [feat] Component headerlogged

---
+++ src/views/Credentials/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex justify-center w-full h-28 bg-brand-main">
+    <header-logged/>
+  </div>
+  <div class="flex flex-col justify-center items-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">
+      Credenciais
+    </h1>
+    <p class="text-lg text-center text-gray-800 fontregular">
+      Guia de instalação e geração de credenciais
+    </p>
+  </div>
+</template>
+
+<script>
+import HeaderLogged from '../../components/HeaderLogged/index.vue'
+export default {
+  components: { HeaderLogged }
+}
+</script>
+<style>
+</style>
