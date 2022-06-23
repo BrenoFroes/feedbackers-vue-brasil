70dc6bcbd3a11d0e7e9e84a34ec82590f28fb419
brenofroes <bfroes58@gmail.com>
2022-06-23 11:53:08 -0300

    [feat] Services feedbacks

---
+++ src/views/Feedbacks/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex justify-center w-full h-28 bg-brand-main">
+    <header-logged/>
+  </div>
+  <div class="flex flex-col justify-center items-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">
+      Feedbacks
+    </h1>
+    <p class="text-lg text-center text-gray-800 fontregular">
+      Detalhes de todos os feedbacks recebidos.
+    </p>
+  </div>
+</template>
+<script>
+import HeaderLogged from '@/components/HeaderLogged'
+export default {
+  components: {
+    HeaderLogged
+  }
+}
+</script>
