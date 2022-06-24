7a689109c27f50814c079e129f2df33ccdd736c9
brenofroes <bfroes58@gmail.com>
2022-06-23 21:08:09 -0300

    [feat] Component filters

--- src/views/Feedbacks/index.vue
+++ src/views/Feedbacks/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-center w-full h-28 bg-brand-main">
-    <header-logged/>
+    <header-logged />
   </div>
   <div class="flex flex-col justify-center items-center h-64 bg-brand-gray">
     <h1 class="text-4xl font-black text-center text-gray-800">
@@ -10,12 +10,30 @@
       Detalhes de todos os feedbacks recebidos.
     </p>
   </div>
+  <div class="flex justify-center w-full pb-20">
+    <div class="w-4/5 max-w-6xl py-10 grid grid-cols-4 gap-2">
+      <div>
+        <h1 class="text-3xl font-black text-brand-darkgray">Listagem</h1>
+        <suspense>
+          <template #default>
+            <filters/>
+          </template>
+          <template #fallback>
+            loading...
+          </template>
+        </suspense>
+      </div>
+      <div class="px-10 pt-20 col-span-3"></div>
+    </div>
+  </div>
 </template>
 <script>
 import HeaderLogged from '@/components/HeaderLogged'
+import Filters from './Filters.vue'
 export default {
   components: {
-    HeaderLogged
+    HeaderLogged,
+    Filters
   }
 }
 </script>
