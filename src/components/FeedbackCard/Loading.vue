fff9870694dc02440e634a2612343073638e7106
brenofroes <bfroes58@gmail.com>
2022-06-24 12:24:44 -0300

    [feat] Component feedback

---
+++ src/components/FeedbackCard/Loading.vue
@@ -0,0 +1,32 @@
+<template>
+  <content-loader
+    class="flex flex-col items-center rounded"
+    width="100%"
+    height="300px"
+  >
+    <content-loader
+      class="mt-3 rounded"
+      widht="90%"
+      height="90px"
+      animation-duration="2s"
+    />
+    <content-loader
+      class="mt-3 rounded"
+      widht="90%"
+      height="90px"
+      animation-duration="2.3s"
+    />
+    <content-loader
+      class="mt-3 rounded"
+      widht="90%"
+      height="90px"
+      animation-duration="2.7s"
+    />
+  </content-loader>
+</template>
+<script>
+import contentLoader from '@/components/ContentLoader'
+export default {
+  components: { contentLoader }
+}
+</script>
